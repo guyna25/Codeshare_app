1760b851700aab628f8dc694646868628170d536
GuyNa <34960387+guyna25@users.noreply.github.com>
2023-06-10 18:46:27 +0300

    fixed bugs in access control

--- src/api/CodingGround.jsx
+++ src/api/CodingGround.jsx
@@ -6,17 +6,41 @@ import { javascript } from '@codemirror/lang-javascript';
 import Header from "./Header";
 import SnippetsContext from "./SnippetsContext";
 
-const CodingGround = (props) => {
+const CodingGround = () => {
   const snippetsCode = useContext(SnippetsContext);
   const {snippetId} = useParams();
   const relevant_code = snippetsCode[snippetId].code
   const [enteredCode, setEnteredCode] = useState(relevant_code);
-  const [accessLevel, setAccessLevel] = useState(()=>'read'); // Initialize access level as 'write'
+  const [accessLevel, setAccessLevel] = useState('read'); // Initialize access level as 'write'
 
   const db = getDatabase();
   const snippetRef = ref(db, `codeSnippets/${snippetId}/code`);
   const activeUsersRef = ref(db, `codeSnippets/${snippetId}/activeUsers`); // Create a reference for activeUsers
-
+  useEffect(() => {
+    console.log("UseEffect start");
+    set(activeUsersRef, increment(1)).then(() => {
+      console.log("Then after increment");
+      get(activeUsersRef).then((snapshot) => {
+        console.log(`Then after activeUsersRef ${snapshot.val()}`);
+        if (snapshot.val() > 1) {
+          console.log(`accessLevel: ${accessLevel}`);
+          setAccessLevel("write");
+        }
+      });
+    });
+    const handleUnload = () => {
+      set(activeUsersRef, increment(-1));
+    };
+  
+    window.addEventListener('beforeunload', handleUnload);
+    return () => {
+      // Decrement the count of active users
+      console.log("Cleanup");
+      set(activeUsersRef, increment(-1));
+      window.removeEventListener('beforeunload', handleUnload);
+    };
+  }, []);
+  
   // Monitor the active users in the coding ground and the current code
   useEffect(() => {
     const listener = onValue(snippetRef, (snapshot) => {
@@ -25,25 +49,12 @@ const CodingGround = (props) => {
         setEnteredCode(codeFromDatabase);
       }
     });
-    
-    set(activeUsersRef, increment(1)).then((value) => {
-      get(activeUsersRef).then((snapshot) => {
-        //if the user is not the first, they can write
-        if (snapshot.val() > 1) {
-          setAccessLevel('write');
-        }
-      });
-    });
 
     return () => {
       // Clean up the listener when the component unmounts
       listener();
-
-      // Decrement the count of active users
-      set(activeUsersRef, increment(-1));
-
     };
-  }, [snippetId, enteredCode, accessLevel]); // Add accessLevel to the dependencies
+  }, [snippetId, enteredCode, snippetRef]);
 
   const enteredCodeChangeHandler = (newCode) => {
     if (accessLevel === 'write') { // Only allow changes if user has write access
@@ -59,8 +70,6 @@ const CodingGround = (props) => {
         });
     }
   };
-
-  console.log(accessLevel === 'read');
   //TODO add codemirror theme
   return (
     <div>
